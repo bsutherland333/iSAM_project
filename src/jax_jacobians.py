53e88befdda7a97e80eb85bd305b0258e8241520
curtispevans <curtispevans@gmail.com>
2024-11-27 12:34:24 -0700

    created the jaxifying module

---
+++ src/jax_jacobians.py
@@ -0,0 +1,21 @@
+import numpy as np
+import jax.numpy as jnp
+from jax import jacfwd, jacrev
+import jax
+from typing import Callable, Tuple
+from numpy.typing import NDArray
+
+# this is to make all the arrays float64
+jax.config.update("jax_enable_x64", True)
+
+def G(x: NDArray) -> NDArray:
+    return -np.eye(len(x))
+
+def make_F_H_J_G(motion_model: Callable, measurement_model: Callable) -> Tuple[Callable, Callable, Callable, Callable]:
+    F = jacfwd(motion_model, argnums=0)
+    H = jacfwd(measurement_model, argnums=0)
+    J = jacfwd(measurement_model, argnums=1)
+
+    return F, H, J, G
+    
+    
