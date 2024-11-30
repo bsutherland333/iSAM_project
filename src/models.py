added unit tests for all jacobians and fixed the models so they can handle jax

src/models.py
@@ -82,12 +82,12 @@ def sensor_model(x: NDArray, landmark: NDArray) -> NDArray:
     del_x = landmark[0] - x[0]
     del_y = landmark[1] - x[1]
 
-    range = np.sqrt(del_x**2 + del_y**2)
-    bearing = np.arctan2(del_y, del_x) - x[2]
+    range = jnp.sqrt(del_x**2 + del_y**2)
+    bearing = jnp.arctan2(del_y, del_x) - x[2]
 
     bearing = _wrap_within_pi(bearing)
 
-    return np.array([range, bearing], dtype=float).reshape(2, 1)
+    return jnp.array([range, bearing], dtype=float).reshape(2, 1)
 
 
 def _wrap_within_pi(val: float) -> float:

tests/factor_graph_manager_test.py
@@ -11,5 +11,31 @@ def test_F_jacobian():
     F, _, _, G = make_F_H_J_G(motion_model, sensor_model)
     x = jnp.array([np.cos(0.), np.sin(0.), 0.]).reshape(3, 1)
     u = jnp.array([0., 0., 0.]).reshape(3, 1)
-    print(F(x, u).reshape(3,3))
+    expected = np.eye(3)
     assert F(x, u).reshape(3,3).shape == (3, 3)
+    assert np.allclose(F(x, u).reshape(3,3), expected)
+
+def test_H_jacobian():
+    F, H, J, G = make_F_H_J_G(motion_model, sensor_model)
+    x = jnp.array([1, 1, 0.]).reshape(3, 1)
+    l = jnp.array([2.,2.]).reshape(2, 1)
+    H_eval = H(x, l).reshape(2,3)
+    H_expected = np.array([[-np.sqrt(2)/2, -np.sqrt(2)/2, 0],
+                           [1/2, -1/2, -1]])
+    assert np.allclose(H_eval, H_expected)
+
+def test_J_jacobian():
+    F, H, J, G = make_F_H_J_G(motion_model, sensor_model)
+    x = jnp.array([1, 1, 0.]).reshape(3, 1)
+    l = jnp.array([2.,2.]).reshape(2, 1)
+    J_eval = J(x,l).reshape(2,2)
+    J_expected = np.array([[np.sqrt(2)/2, np.sqrt(2)/2],
+                           [-1/2, 1/2]])
+    assert np.allclose(J_eval, J_expected)
+
+def test_G_jacobian():
+    F, H, J, G = make_F_H_J_G(motion_model, sensor_model)
+    x = jnp.array([1, 1, 0.]).reshape(3, 1)
+    G_eval = G(x)
+    G_expected = -np.eye(3)
+    assert np.allclose(G_eval, G_expected)