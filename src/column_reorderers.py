Fixed error with incorrect functions used for odometry jacobians

src/column_reorderers.py
@@ -17,10 +17,24 @@ def colamd(A: NDArray) -> Tuple[NDArray, NDArray]:
     assert A.ndim == 2
     assert A.dtype == np.float64
 
-    # P = np.eye(A.shape[1], dtype=int)
-
     p = splu(A.T @ A, permc_spec='COLAMD').perm_c
     P = np.zeros((A.shape[1], A.shape[1]), dtype=int)
     P[np.arange(np.shape(A)[1]), p] = 1
 
     return A @ P, P
+
+
+def passthrough(A: NDArray) -> Tuple[NDArray, NDArray]:
+    """
+    Passthrough function for the reordering algorithm. Does nothing.
+
+    Parameters:
+    A: The matrix to reorder.
+
+    Returns:
+    The original matrix A and the identity matrix.
+    """
+    assert A.ndim == 2
+    assert A.dtype == np.float64
+
+    return A, np.eye(A.shape[1], dtype=int)

src/factor_graph_manager.py
@@ -118,8 +118,8 @@ class FactorGraphManager:
             previous = current - self.dim_state
             next = current + self.dim_state
             F_evaluated = F(x[previous:current], u).reshape(self.dim_state, self.dim_state)
-            G_evaluated = G(x[current:next])
-            b[current:next] = self.sqrt_inv_odometry_cov @ (u - self.inverse_odometry_model(x[previous:current], x[current:next]))
+            G_evaluated = G(x[previous:current], x[current:next]).reshape(self.dim_state, self.dim_state)
+            b[current:next] = self.sqrt_inv_odometry_cov @ (u - np.array(self.inverse_odometry_model(x[previous:current], x[current:next])))
             A[current:next, previous:current] = self.sqrt_inv_odometry_cov @ F_evaluated
             A[current:next, current:next] = self.sqrt_inv_odometry_cov @ G_evaluated
 

src/jax_jacobians.py
@@ -8,14 +8,11 @@ from numpy.typing import NDArray
 # this is to make all the arrays float64
 jax.config.update("jax_enable_x64", True)
 
-def G(x: NDArray) -> NDArray:
-    return -np.eye(len(x))
-
-def make_F_H_J_G(motion_model: Callable, measurement_model: Callable) -> Tuple[Callable, Callable, Callable, Callable]:
-    F = jacfwd(motion_model, argnums=0)
+def make_F_H_J_G(inverse_motion_model: Callable, measurement_model: Callable) -> Tuple[Callable, Callable, Callable, Callable]:
+    F = jacfwd(inverse_motion_model, argnums=0)
+    G = jacfwd(inverse_motion_model, argnums=1)
     H = jacfwd(measurement_model, argnums=0)
     J = jacfwd(measurement_model, argnums=1)
 
     return F, H, J, G
-    
-    
+

src/main.py
@@ -18,6 +18,8 @@ from qr_factorizers import np_qr as qr
 from solvers import scipy_solver as solver
 from solvers import iterative_linear_least_squares as non_linear_solve
 
+# Set numpy to not wrap arrays
+np.set_printoptions(linewidth=np.inf)
 
 def main(num_iterations: int, data_filepath: str, use_iterative_solver: bool, num_iters_before_batch: int, plot_live: bool):
     range_measurement_std = 0.1
@@ -41,7 +43,7 @@ def main(num_iterations: int, data_filepath: str, use_iterative_solver: bool, nu
                          odometry_translation_std=odom_translation_std,
                          odometry_rotation_std=odom_rotation_std)
 
-    F, H, J, G = make_F_H_J_G(motion_model, sensor_model)
+    F, H, J, G = make_F_H_J_G(inverse_motion_model, sensor_model)
     factor_graph_manager = FactorGraphManager(inverse_motion_model,
                                               sensor_model,
                                               x,

src/models.py
@@ -3,7 +3,7 @@ import jax.numpy as jnp
 from jax import jit
 from numpy.typing import NDArray
 
-@jit
+
 def motion_model(x: NDArray, u: NDArray) -> NDArray:
     """
     Motion model for our system. Uses basic "rotate, move, rotate" model from chapter 5.4 of
@@ -29,10 +29,11 @@ def motion_model(x: NDArray, u: NDArray) -> NDArray:
 
     heading_next = _wrap_within_pi(heading_next)
 
-    return jnp.array([x_next, y_next, heading_next], dtype=float).reshape(3, 1)
+    return np.array([x_next, y_next, heading_next], dtype=float).reshape(3, 1)
 
 
-def inverse_motion_model(x: NDArray, x_next: NDArray) -> NDArray:
+@jit
+def inverse_motion_model(x: NDArray, x_next: NDArray):
     """
     Inverse motion model of our system. Like the motion model, but instead of getting the next
     state given the current and control input, we get the control input given the current and next
@@ -53,17 +54,17 @@ def inverse_motion_model(x: NDArray, x_next: NDArray) -> NDArray:
     del_x = x_next[0] - x[0]
     del_y = x_next[1] - x[1]
 
-    dist = np.sqrt(del_x**2 + del_y**2)
-    rot1 = np.arctan2(del_y, del_x) - x[2]
+    dist = jnp.sqrt(del_x**2 + del_y**2)
+    rot1 = jnp.arctan2(del_y, del_x) - x[2]
     rot2 = x_next[2] - x[2] - rot1
 
     rot1 = _wrap_within_pi(rot1)
     rot2 = _wrap_within_pi(rot2)
 
-    return np.array([rot1, dist, rot2], dtype=float).reshape(3, 1)
+    return jnp.array([rot1, dist, rot2], dtype=float).reshape(3, 1)
 
 
-def sensor_model(x: NDArray, landmark: NDArray) -> NDArray:
+def sensor_model(x: NDArray, landmark: NDArray):
     """
     Sensor model for our system. Uses a basic range and bearing model.
 

src/simulator.py
@@ -145,7 +145,7 @@ class Simulator:
         measurements = np.hstack(measurements)
 
         # Get the odometry reading
-        odometry = self._inverse_motion_model(self._prev_x, next_state)
+        odometry = np.array(self._inverse_motion_model(self._prev_x, next_state))
         self._prev_x = next_state
         odometry[0] += np.random.normal(self._odometry_rotation_bias, self._odometry_rotation_std)
         odometry[1] += np.random.normal(self._odometry_translation_bias, self._odometry_translation_std)